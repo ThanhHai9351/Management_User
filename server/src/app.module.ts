2ab1d74fb7b39d7b25451b081c818c05ef991fe9
ThanhHai9351 <haihailua123456@gmail.com>
2025-03-24 09:42:56 +0700

    feat: server models

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from '@/modules/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LikesModule } from '@/modules/likes/likes.module';
+import { MenuItemsModule } from '@/modules/menu.items/menu.items.module';
+import { MenusModule } from '@/modules/menus/menus.module';
+import { RestaurantsModule } from '@/modules/restaurants/restaurants.module';
+import { MenuItemOptionsModule } from '@/modules/menu.item.options/menu.item.options.module';
+import { OrderDetailModule } from '@/modules/order.detail/order.detail.module';
+import { OrdersModule } from '@/modules/orders/orders.module';
+import { ReviewsModule } from '@/modules/reviews/reviews.module';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ManagementUser',
+    ),
+    UsersModule,
+    LikesModule,
+    MenuItemsModule,
+    MenusModule,
+    RestaurantsModule,
+    MenuItemOptionsModule,
+    OrderDetailModule,
+    OrdersModule,
+    ReviewsModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  app.enableCors();
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
+
+  await app.listen(port || 5000);
 }
 bootstrap();
